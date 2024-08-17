fd67e48a99906a338558da425b36f06bd65ae323
ADI9325 <adityabagade04@gmail.com>
2024-08-17 09:53:44 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,5 @@
+// alert("hey welocome in my website");
+const id =document.getElementById("id");
+const heading = document.getElementById("heading")
+console.log(heading.innerHTML);
+const changeHEading = heading.innerHTML;
